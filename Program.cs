9942052bc9aec374809cd3b8f56dca2f701e254f
Liisbet V. <liisbet.vaiksaar@tktk.ee>
2024-04-05 17:24:27 +0300

    create new app

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// //This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name: ");
+string userName;
+
+userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+Console.WriteLine($"Hello, {userName} !");
